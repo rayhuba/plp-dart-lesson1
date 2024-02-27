56df7b070dfc5c4fb20507bb819ee8e8360400df
rayhuba <huba855@gmail.com>
2024-02-28 00:49:40 +0300

    i have added an extra comment in my code

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("Hello world!");
+  //declaring variables
+String name ="John";  //must be in quotes
+String address = "Kenya";
+num age = 20; //whole number
+num height = 5.9; //decimal numbers
+bool isMarried = false; //boolean
+
+//printing variables value
+print("His/Her name is $name");
+print("Address is $address");
+print("Age is $age");
+print("Height is $height");
+print("Married Status is $isMarried");
+} 
